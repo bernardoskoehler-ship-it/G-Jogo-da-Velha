a9329783f934dd8d51df1849bb199dfe273916be
Koehler <bernardoskoehler@gmail.com>
2026-04-10 14:48:35 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -9,3 +9,11 @@ public class Jogo {
     grade[pos1][pos2] = 3;
     System.out.println(grade[pos1][pos2]);
 }
+
+System.out.println("Digite as suas posições");
+        Controle controle = new Controle();
+        int pos1 = Controle.lerInt();
+        int pos2 = Controle.lerInt();
+
+        grade[pos1][pos2] = 3;
+        System.out.println(grade[pos1][pos2]);
