0483e603205036369983cca4dd9d473343beda1a
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 16:22:41 -0300

    Protótipo Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Jogo jogo = new Jogo();
         Controle c = new Controle();
-        Jogador j = new Jogador("jogador", 2);
-        jogo.pegarPosicao(c, j);
-        jogo.mostrarGrade();
+        Jogador jogador = new Jogador("jogador", 1);
+        Jogador jogador2 = new Jogador("jogador", 2);
+
+        jogo.iniciarJogo(jogador, jogador2);
+
     }
 }
