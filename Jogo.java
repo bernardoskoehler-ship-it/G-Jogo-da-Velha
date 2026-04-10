fec2e17c77dda4810e996c5253816f5fafe65d89
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 16:01:45 -0300

    Grade atualizada
    Especificadores de posição adicionados.

--- Jogo.java
+++ Jogo.java
@@ -2,30 +2,31 @@ public class Jogo {
      int[][] grade = new int[3][3];
 
 
-    public void mostrarGrade(){
-     System.out.println("====================================");
-
-
-     System.out.println(mostrarSimbolo(grade[0][0])+" | "+mostrarSimbolo(grade[0][1])+" | "+mostrarSimbolo(grade[0][2]));
-     System.out.println(mostrarSimbolo(grade[1][0])+" | "+mostrarSimbolo(grade[1][1])+" | "+mostrarSimbolo(grade[1][2]));
-     System.out.println(mostrarSimbolo(grade[2][0])+" | "+mostrarSimbolo(grade[2][1])+" | "+mostrarSimbolo(grade[2][2]));
-
-
-     System.out.println("====================================");
-    }
-     public String mostrarSimbolo(int i) {
-          if(i == 1) return "X";
-          if(i == 2) return "O";
-          return ".";
-        }
-
-    public void pegarPosicao(Controle controle){
+     public void mostrarGrade(){
+        System.out.println("====================================");
+   
+        System.err.println("  0   1   2");
+        
+        System.out.println("0 "+mostrarSimbolo(grade[0][0])+" | "+mostrarSimbolo(grade[0][1])+" | "+mostrarSimbolo(grade[0][2]));
+        System.out.println("1 "+mostrarSimbolo(grade[1][0])+" | "+mostrarSimbolo(grade[1][1])+" | "+mostrarSimbolo(grade[1][2]));
+        System.out.println("2 "+mostrarSimbolo(grade[2][0])+" | "+mostrarSimbolo(grade[2][1])+" | "+mostrarSimbolo(grade[2][2]));
+   
+   
+        System.out.println("====================================");
+       }
+        public String mostrarSimbolo(int i) {
+             if(i == 1) return "X";
+             if(i == 2) return "O";
+             return ".";
+           }
+    public void pegarPosicao(Controle controle, Jogador jogador){
         System.out.println("Digite as suas posições");
         int pos1 = Controle.lerInt();
         int pos2 = Controle.lerInt();
-        grade[pos1][pos2] = 1;
+        grade[pos1][pos2] = jogador.simbolo;
     }
-     public boolean espacoLivre(int linha, int coluna) {
-                return grade[linha][coluna] == 0;
-        }
+
+    public boolean espacoLivre(int linha, int coluna) {
+        return grade[linha][coluna] == 0;
+}
 }
