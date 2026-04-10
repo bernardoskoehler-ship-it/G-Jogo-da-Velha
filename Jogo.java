f6488a9a9af49f39e5d65f0bd30e81168b304287
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 14:36:20 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -1,3 +1,11 @@
 public class Jogo {
     int[][] grade = new int[3][3];
+
+    //Pegar posições
+    System.out.println("Digite as suas posições");
+    int pos1 = controle.lerInt();
+    int pos2 = controle.lerInt();
+    
+    grade[pos1][pos2] = 3;
+    System.out.println(grade[pos1][pos2]);
 }
