260ab7b7625af51a6fda9077a4a1054b222e59b6
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 16:23:28 -0300

    Protótipo Jogador.java lançado

--- Jogador.java
+++ Jogador.java
@@ -1,17 +1,13 @@
 public class Jogador {
-	//so resenha isso mds
-	String nome;
-	int simbolo;
-	Jogador(String nome, int simbolo) {
-	    this.nome = nome;
-	    setSimbolo(simbolo);
-	}
-	private void setSimbolo(int simbolo) {
-	    if(simbolo >= 1 || simbolo <= 2) {
-	         this.simbolo = simbolo;
-	    }else {
-	        this.simbolo = 1;
-	     }
-	    }
-	    
+    String nome;
+    int simbolo;
+    Jogador(String nome, int simbolo) {
+        this.nome = nome;
+        setSimbolo(simbolo);
+    }
+    private void setSimbolo(int simbolo) {
+        if(simbolo >= 1 || simbolo <= 2) {
+            this.simbolo = simbolo;
+        }
+    }
 }
