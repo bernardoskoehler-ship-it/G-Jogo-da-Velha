2c3af03c7e6aa95ec4354d5c13760416d671838e
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 14:29:57 -0300

    Controle adicionado

---
+++ controle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class controle {
+    private static Scanner input = new Scanner(System.in);
+
+    public static int lerInt(){
+        return input.nextInt();
+    }
+}
