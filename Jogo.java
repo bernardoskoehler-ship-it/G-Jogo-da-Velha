fa3d4a39b0fc756071d30e62f989868b7bad38a1
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-10 15:10:34 -0300

    Função de marcar posição adicionada a Jogo.java
    Função de marcar posição adicionada.

--- Jogo.java
+++ Jogo.java
@@ -1,19 +1,34 @@
 public class Jogo {
-    int[][] grade = new int[3][3];
+    static int[][] grade = new int[3][3];
 
-    //Pegar posições
-    System.out.println("Digite as suas posições");
-    int pos1 = controle.lerInt();
-    int pos2 = controle.lerInt();
-    
-    grade[pos1][pos2] = 3;
-    System.out.println(grade[pos1][pos2]);
-}
 
-System.out.println("Digite as suas posições");
+    static public void mostrarGrade(){
+        System.out.println("====================================");
+
+
+        System.out.println(grade[0][0]+" | "+grade[0][1]+" | "+grade[0][2]);
+        System.out.println(grade[1][0]+" | "+grade[1][1]+" | "+grade[1][2]);
+        System.out.println(grade[2][0]+" | "+grade[2][1]+" | "+grade[2][2]);
+
+
+        System.out.println("====================================");
+    }
+
+    public static void main(String[] args) {
+        
+
+
+        System.out.println("Digite as suas posições");
         Controle controle = new Controle();
         int pos1 = Controle.lerInt();
         int pos2 = Controle.lerInt();
 
-        grade[pos1][pos2] = 3;
-        System.out.println(grade[pos1][pos2]);
+        grade[pos1][pos2] = 1;
+
+        mostrarGrade();
+        
+
+    }
+
+
+}
