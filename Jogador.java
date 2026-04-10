d89bf3def45403888e459204597f31d58da89740
estrangeiro32 <miguelmikoseit@gmail.com>
2026-04-10 15:50:33 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,13 +1,17 @@
 public class Jogador {
-    String nome;
-    int simbolo;
-    Jogador(String nome, int simbolo) {
-        this.nome = nome;
-        setSimbolo(simbolo);
-    }
-    private void setSimbolo(int simbolo) {
-        if(simbolo >= 1 || simbolo <= 2) {
-            this.simbolo = simbolo;
-        }
-    }
+	//so resenha isso mds
+	String nome;
+	int simbolo;
+	Jogador(String nome, int simbolo) {
+	    this.nome = nome;
+	    setSimbolo(simbolo);
+	}
+	private void setSimbolo(int simbolo) {
+	    if(simbolo >= 1 || simbolo <= 2) {
+	         this.simbolo = simbolo;
+	    }else {
+	        this.simbolo = 1;
+	     }
+	    }
+	    
 }
